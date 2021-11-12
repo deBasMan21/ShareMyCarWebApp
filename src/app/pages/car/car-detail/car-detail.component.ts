60f7f7f6bca1f73531c632dba46b43a91bec3605
Bas <bbuijsen@gmail.com>
2021-11-12 16:16:46 +0100

    finished cardetail for now

--- src/app/pages/car/car-detail/car-detail.component.ts
+++ src/app/pages/car/car-detail/car-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Ride } from '../../ride/ride.model';
 import { Car } from '../car.model';
 
 @Component({
@@ -11,6 +12,18 @@ export class CarDetailComponent implements OnInit {
   public id: number | null = null;
 
   public car: Car = { id: 1, name: 'Tesla model 3', plate: 'HX-803-F', imgSrc: 'https://www.pngall.com/wp-content/uploads/7/White-Tesla-Electric-Car-PNG-Picture.png' };
+  public rides: Ride[] = [
+    { id: 1, name: "maccie", beginDateTime: new Date('12-11-2021 12:00'), endDateTime: new Date('12-11-2021 15:00') },
+    { id: 1, name: "maccie", beginDateTime: new Date('12-11-2021 12:00'), endDateTime: new Date('12-11-2021 15:00') },
+    { id: 1, name: "maccie", beginDateTime: new Date('12-11-2021 12:00'), endDateTime: new Date('12-11-2021 15:00') },
+    { id: 1, name: "maccie", beginDateTime: new Date('12-11-2021 12:00'), endDateTime: new Date('12-11-2021 15:00') },
+    { id: 1, name: "maccie", beginDateTime: new Date('12-11-2021 12:00'), endDateTime: new Date('12-11-2021 15:00') },
+    { id: 1, name: "maccie", beginDateTime: new Date('12-11-2021 12:00'), endDateTime: new Date('12-11-2021 15:00') },
+    { id: 1, name: "maccie", beginDateTime: new Date('12-11-2021 12:00'), endDateTime: new Date('12-11-2021 15:00') },
+    { id: 1, name: "maccie", beginDateTime: new Date('12-11-2021 12:00'), endDateTime: new Date('12-11-2021 15:00') },
+    { id: 1, name: "maccie", beginDateTime: new Date('12-11-2021 12:00'), endDateTime: new Date('12-11-2021 15:00') },
+    { id: 1, name: "maccie", beginDateTime: new Date('12-11-2021 12:00'), endDateTime: new Date('12-11-2021 15:00') },
+  ]
 
   constructor(public route: ActivatedRoute) { }
 
