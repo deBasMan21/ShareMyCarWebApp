8c00985fb2fec1882713cb7e381e48763b33b38c
Bas <bbuijsen@gmail.com>
2021-11-12 15:31:58 +0100

    worked further on cardetails

--- src/app/pages/car/car-detail/car-detail.component.ts
+++ src/app/pages/car/car-detail/car-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Car } from '../car.model';
 
 @Component({
   selector: 'app-car-detail',
@@ -8,6 +9,8 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CarDetailComponent implements OnInit {
   public id: number | null = null;
+
+  public car: Car = { id: 1, name: 'Tesla model 3', plate: 'HX-803-F', imgSrc: 'https://www.pngall.com/wp-content/uploads/7/White-Tesla-Electric-Car-PNG-Picture.png' };
 
   constructor(private route: ActivatedRoute) { }
 
