805c3ee173ba97130748e9acac785213c3538c23
basbuijsen <bbuijsen@gmail.com>
2021-11-16 12:49:18 +0100

    form input fields are filled in automatically

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './pages/user/user-list/user-list.component';
@@ -36,7 +36,7 @@ import { LocationEditComponent } from './pages/location/location-edit/location-e
     LocationDetailComponent,
     LocationEditComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/car/car-edit/car-edit.component.ts
+++ src/app/pages/car/car-edit/car-edit.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Car } from '../car.model';
+import { ActivatedRoute } from '@angular/router';
+import { CarServiceService } from 'src/app/services/car-service.service';
 
 @Component({
   selector: 'app-car-edit',
   templateUrl: './car-edit.component.html',
-  styleUrls: ['./car-edit.component.scss']
+  styleUrls: ['./car-edit.component.scss'],
 })
 export class CarEditComponent implements OnInit {
+  public id: number = 0;
+  public car: Car = { name: 'test', id: 0, imgSrc: '', plate: '' };
 
-  constructor() { }
+  constructor(
+    public route: ActivatedRoute,
+    private carService: CarServiceService
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: any) => {
+      this.id = params['id'];
+      this.car = this.carService.getCarById(this.id)!;
+      console.log(this.car);
+    });
   }
-
 }
