3f6d0dc6965502e0554072d2c1198e1d5451432e
Bas <bbuijsen@gmail.com>
2021-11-22 16:44:44 +0100

    loguit button

--- src/app/pages/user/account/account.component.ts
+++ src/app/pages/user/account/account.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-account',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthenticationService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  logOut() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
