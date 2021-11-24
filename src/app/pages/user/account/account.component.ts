66fd294037115ae7dd068ef3dd4e166872114d78
Bas <bbuijsen@gmail.com>
2021-11-24 14:14:25 +0100

    changed account page

--- src/app/pages/user/account/account.component.ts
+++ src/app/pages/user/account/account.component.ts
@@ -10,6 +10,7 @@ import { User } from '../user.model';
 })
 export class AccountComponent implements OnInit {
   public user: User = { name: '', email: '', phoneNumber: '', cars: [] }
+  public expireDateTime: any = { days: 0, hours: 0, minutes: 0 };
 
   constructor(private authService: AuthenticationService,
     private router: Router) { }
@@ -19,6 +20,13 @@ export class AccountComponent implements OnInit {
       this.user = user;
       console.log((user));
     });
+    const time: number = - new Date().getTime();
+    let endDate = new Date();
+    let purchaseDate = this.authService.getExpiration()?.getTime()!;
+    let diffMs = (purchaseDate.valueOf() - endDate.valueOf()); // milliseconds
+    this.expireDateTime.days = Math.floor(diffMs / 86400000); // days
+    this.expireDateTime.hours = Math.floor((diffMs % 86400000) / 3600000); // hours
+    this.expireDateTime.minutes = Math.round(((diffMs % 86400000) % 3600000) / 60000) - 1; // minutes
   }
 
   logOut() {
